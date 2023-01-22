c78db40476e641904acc468a387b989f89f6ba43
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-01-22 09:59:22 +0100

    feat: reorganize operation resolver

--- service-worker-scripts/operation-resolver.js
+++ service-worker-scripts/operation-resolver.js
@@ -9,41 +9,98 @@ import { CloseWindowOperation } from '/service-worker-scripts/operations/close-w
 import { MinimizeWindowOperation } from '/service-worker-scripts/operations/minimize-window-operation.js';
 
 export class OperationResolver {
+  #operations = {
+    goBack: {
+      operation: new GoBackOperation(),
+      label: 'Go Back',
+      isAsync: false
+    },
+    goForward: {
+      operation: new GoForwardOperation(),
+      label: 'Go Forward',
+      isAsync: false
+    },
+    openNewTab: {
+      operation: new OpenNewTabOperation(),
+      label: 'Open New Tab',
+      isAsync: true
+    },
+    closeCurrentTab: {
+      operation: new CloseCurrentTabOperation(),
+      label: 'Close Current Tab',
+      isAsync: true
+    },
+    reloadCurrentTab: {
+      operation: new ReloadCurrentTabOperation(),
+      label: 'Reload Current Tab',
+      isAsync: false
+    },
+    switchToLeftTab: {
+      operation: new SwitchToLeftTabOperation(),
+      label: 'Switch to Left Tab',
+      isAsync: true
+    },
+    switchToRightTab: {
+      operation: new SwitchToRightTabOperation(),
+      label: 'Switch to Right Tab',
+      isAsync: true
+    },
+    closeWindow: {
+      operation: new CloseWindowOperation(),
+      label: 'Close Window',
+      isAsync: true
+    },
+    minimizeWindow: {
+      operation: new MinimizeWindowOperation(),
+      label: 'Minimize Window',
+      isAsync: true
+    }
+  }
+
+  #map = {
+    'left': 'goBack',
+    'right': 'goForward',
+    'up': 'openNewTab',
+    'down': 'closeCurrentTab',
+    'up|down': 'reloadCurrentTab',
+    'up|left': 'switchToLeftTab',
+    'up|right': 'switchToRightTab',
+    'left|down|right': 'closeWindow',
+    'down|left': 'minimizeWindow'
+  }
+
+  getList() {
+    const elements = [];
+    for (const operationKey in this.#operations) {
+      if (Object.hasOwnProperty.call(this.#operations, operationKey)) {
+        const element = this.#operations[operationKey];
+        elements.push({
+          value: operationKey,
+          label: element.label
+        });
+      }
+    }
+
+    return elements;
+  }
+
   async resolveAsync(directions) {
     const serializedDirections = this.#serializeDirections(directions);
 
-    switch (serializedDirections) {
-      case 'left':
-        new GoBackOperation().do();
-        break;
-      case 'right':
-        new GoForwardOperation().do();
-        break;
-      case 'up':
-        await new OpenNewTabOperation().doAsync();
-        break;
-      case 'down':
-        await new CloseCurrentTabOperation().doAsync();
-        break;
-      case 'up|down':
-        new ReloadCurrentTabOperation().do();
-        break;
-      case 'up|left':
-        await new SwitchToLeftTabOperation().doAsync();
-        break;
-      case 'up|right':
-        await new SwitchToRightTabOperation().doAsync();
-        break;
-      case 'left|down|right':
-        await new CloseWindowOperation().doAsync();
-        break;
-      case 'down|left':
-        await new MinimizeWindowOperation().doAsync();
-        break;
-      default:
-        console.warn(serializedDirections);
-        break;
+    if (!this.#map.hasOwnProperty(serializedDirections)) {
+      console.warn(serializedDirections);
+
+      return;
     }
+
+    if (!this.#operations.hasOwnProperty(this.#map[serializedDirections])) {
+      console.warn(serializedDirections);
+      
+      return;
+    }
+
+    const el = this.#operations[this.#map[serializedDirections]];
+    el.isAsync ? await el.operation.doAsync() : el.operation.do();
   }
 
   #serializeDirections(directions) {
