9458b99b8f6a22e79ace25ffca210b74286ae44d
qwertfisch <develop-git@qwertfisch.eu>
2025-01-27 19:23:32 +0100

    FIX: open new empty window, similar to "open new tab"
    Opens a new window with an empty tab, the same size as the previous current window.
    This operation shall not duplicate the current tab as new window.

--- service-worker-scripts/operations/open-new-window-operation.js
+++ service-worker-scripts/operations/open-new-window-operation.js
@@ -1,11 +1,5 @@
 export class OpenNewWindowOperation {
   async doAsync() {
-    const currentTab = await chrome.tabs.query({ active: true, currentWindow: true });
-    let url = '';
-    if (currentTab?.length > 0 && currentTab[0].url) {
-      url = currentTab[0].url;
-    }
-
-    chrome.windows.create({ url, state: 'maximized' });
+    chrome.windows.create();
   }
 }
